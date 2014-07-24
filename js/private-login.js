2bc6956e0b7335884dd5d1f58be12b97fb6d7918
Tocher <tocher99@gmail.com>
2014-07-24 14:07:59 +0300

    validation fix

--- js/private-login.js
+++ js/private-login.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 		$("#main-l").css("min-height",$(window).height() - $("header").height() - $("footer").height());
 	})
 
-	$("#login-form").validate({
+	$(".plogin-form").validate({
 		rules: {
 		    Domain: {
 		      required: true
@@ -15,7 +15,53 @@ $(document).ready(function() {
 		      required: true,
 		      email: true
 		    }
-		  }
+		  },
+		 messages: {
+		    name: "",
+		    email: {
+		      required: "",
+		      email: ""
+		    }
+		  },
+		success: function(label) {
+			if($(label).parent().find('input').hasClass('valid'))
+		    	label.addClass("valid").text("");
+		  },
+	});
+
+	$(".reg-form").validate({
+		rules: {
+		    name: {
+		      required: true
+		    },
+		    email: {
+		      required: true,
+		      email: true
+		    },
+		    password: {
+		    	required: true
+		    },
+		    number: {
+		    	required: true
+		    },
+		    orgname: {
+		    	required: true
+		    },
+		    subdomain: {
+		    	required: true
+		    }
+		  },
+		 messages: {
+		    name: "",
+		    email: {
+		      required: "",
+		      email: ""
+		    }
+		  },
+		success: function(label) {
+			if($(label).parent().find('input').hasClass('valid'))
+		    	label.addClass("valid").text("");
+		  },
 	});
 
 	
